eeccbcb770b65a9c261bbf8f9c2b901295b77fde
DiegoDominici <diego.dominici@gmail.com>
2023-10-14 18:35:54 -0300

    Botones Agregar y Eliminar en cada Pizza y/o Combo

--- js/funciones.js
+++ js/funciones.js
@@ -3,3 +3,15 @@
 function testLlamada() {
     console.log("La función testLlamada se realizó con éxito")
 }
+
+function agregarPizza(numero) {
+    let x = document.getElementsByClassName("cantArticulo")
+    x[numero].innerText = parseInt(x[numero].innerText) + 1
+}
+
+function eliminarPizza(numero) {
+    let x = document.getElementsByClassName("cantArticulo")
+    if (parseInt(x[numero].innerText) > 0) {
+        x[numero].innerText = parseInt(x[numero].innerText) - 1
+    }
+}
