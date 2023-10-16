4b6c95881d0dda7351bf772d6fa8d85d753b8909
DiegoDominici <diego.dominici@gmail.com>
2023-10-16 14:47:02 -0300

    Incorporación CAPTCHA Google

--- js/funciones.js
+++ js/funciones.js
@@ -14,12 +14,35 @@ function agregarPizza(numero) {
     if (total < 10) {
         x[numero].innerText = parseInt(x[numero].innerText) + 1
     }
-    localStorage.setItem("Total", arregloCantidad)
+    for (let i = 0; i < 12; i++) {
+         arregloCantidad.push(parseInt(x[i].innerText))
+    }
+    localStorage.setItem("Total", JSON.stringify(arregloCantidad))
 }
 
 function eliminarPizza(numero) {
     let x = document.getElementsByClassName("cantArticulo")
+    let arregloCantidad = []
     if (parseInt(x[numero].innerText) > 0) {
         x[numero].innerText = parseInt(x[numero].innerText) - 1
     }
+    for (let i = 0; i < 12; i++) {
+        arregloCantidad.push(parseInt(x[i].innerText))
+   }
+   localStorage.setItem("Total", JSON.stringify(arregloCantidad))
 }
+
+function mostrarPedido() {
+    let listaPedidoCompleta = document.getElementById("listaPedido")
+    let listaPizzas = ["Pizza 1", "Pizza 2", "Pizza 3", "Pizza 4", "Pizza 5", "Pizza 6", "Pizza 7", "Pizza 8", "Pizza 9", "Pizza 10", "Pizza 11", "Pizza 12"]
+    let cantidadesPizzas = localStorage.getItem("Total")
+    cantidadesPizzas = JSON.parse(cantidadesPizzas)
+    for (i = 0; i < cantidadesPizzas.length; i++) {
+        if (cantidadesPizzas[i] != "0") {
+            let ingreso = document.createElement("li")
+            ingreso.innerText = cantidadesPizzas[i] + " x " + listaPizzas[i]
+            listaPedidoCompleta.appendChild(ingreso)
+        }
+        
+    }
+    }
